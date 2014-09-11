b3b01b2d8b9408a3dcdc7046e00ef771f9f3f904
Nikolai Manek <niko.manek@gmail.com>
2014-09-10 19:21:57 -0700

    more c_user_id + relations back to pages

--- app/models/facebook_credential.rb
+++ app/models/facebook_credential.rb
@@ -1,5 +1,6 @@
 class FacebookCredential < ActiveRecord::Base
   belongs_to :account
   belongs_to :organization
-  attr_accessible :account_id, :c_user_id, :oauth_token, :oauth_secret, :expires, :organization_id, :facebook_id
+  belongs_to :facebook_page_credential
+  attr_accessible :account_id, :c_user_id, :access_token, :expires, :organization_id, :facebook_id
 end
--- app/models/facebook_page_credential.rb
+++ app/models/facebook_page_credential.rb
@@ -1,6 +1,7 @@
 class FacebookPageCredential < ActiveRecord::Base
   belongs_to :account
   belongs_to :organization
-  attr_accessible :account_id, :name, :category, :access_token, :perms, :organization_id, :fb_id,:has_token
+  has_many :facebook_credentials
+  attr_accessible :account_id, :name, :category, :access_token, :perms, :organization_id, :fb_id,:has_token, :c_user_id
   validates_presence_of :account_id, :access_token, :name, :perms
 end
