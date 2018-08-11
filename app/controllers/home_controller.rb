d889bebfadbce0ea01b9c837ed0c0b70e3f33547
nikoma <niko.manek@gmail.com>
2018-08-11 13:23:42 -0700

    remove idea

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    p "what"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Api::Application.routes.draw do
 
 
+  get '/', to: 'home#index'
+
   namespace :api, defaults: {format: 'json'} do
 
     namespace :v1 do
